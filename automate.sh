61c5daf49e316d01c097fff690e94824358049d0
root <root@localhost.localdomain>
2019-09-08 18:07:21 +0530

    four

--- automate.sh
+++ automate.sh
@@ -2,22 +2,22 @@
 
 kubectl create namespace logging
 
-kubectl create -f ./yaml_files/elastic.yaml -n logging
+kubectl create -f ./kibana_server/yaml_files/elastic.yaml -n logging
 
 kubectl get pods -n logging
 
 kubectl get service -n logging
 
-kubectl create -f ./yaml_files/kibana.yaml -n logging
+kubectl create -f ./kibana_server/yaml_files/kibana.yaml -n logging
 
 kubectl get pods -n logging
 
 kubectl get service -n logging
 
-kubectl create -f ./yaml_files/fluentd-rbac.yaml
+kubectl create -f ./kibana_server/yaml_files/fluentd-rbac.yaml
 
 mkdir -p /var/log/varlog
 
 mkdir -p /var/lib/docker/containers
 
-kubectl create -f ./yaml_files/fluentd-daemonset.yaml
+kubectl create -f ./kibana_server/yaml_files/fluentd-daemonset.yaml
